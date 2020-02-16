545586ff35cc3e9aee7b0fef0dc2ba170802eec0
tkspuk <aoyunlw@163.com>
2020-02-16 15:58:54 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from flask import escape
+# -*- coding: utf-8-*-
+
+from flask import Flask, escape, url_for
 
 app = Flask(__name__)
 
@@ -8,11 +9,24 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s' % escape(name)
 
 
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    print(url_for('hello_world'))  # 输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
+
+
 if __name__ == '__main__':
     app.run()
-
