23070c5311c961ab3c63af8ea9b094401e7217b0
tkspuk <aoyunlw@163.com>
2020-02-16 15:42:12 +0800

    Init project

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import escape
 
 app = Flask(__name__)
 
@@ -6,6 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % escape(name)
 
 
 if __name__ == '__main__':
